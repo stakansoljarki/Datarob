61afdb3895df3dfb12570a159fabdbbe28743c2a
anttibull <finezza15@gmail.com>
2020-01-15 18:59:03 +0200

    fix: accessibility

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,18 +2,20 @@ import { header } from "../../components/Header/Header";
 
 import { slider } from "./../../components/Contact-slider/Contact-slider";
 import SmoothScroll from "smooth-scroll";
-import scrollReveal from "scrollreveal";
 
 import "./Home.sass";
 
 document.addEventListener("DOMContentLoaded", () => {
   header();
-
-  /*
-   * ScrollReveal().reveal(".headline");
-   * ScrollReveal().reveal(".tagline", { delay: 500 });
-   * ScrollReveal().reveal(".punchline", { delay: 2000 });
-   */
   slider();
-  new SmoothScroll('a[href*="#"]');
 });
+document.addEventListener(
+  "touchstart",
+  () => {
+    new SmoothScroll('a[href*="#"]');
+  },
+  {
+    capture: true,
+    passive: true
+  }
+);
